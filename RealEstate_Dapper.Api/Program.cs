98cb1dfa08f9c0ba4ee604650195b8fbfbbc90f9
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-11-03 21:02:47 +0300

    .

--- RealEstate_Dapper.Api/Program.cs
+++ RealEstate_Dapper.Api/Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<Context>();
 builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
