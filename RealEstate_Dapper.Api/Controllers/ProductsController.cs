d59283689e627b3cd79be2fdbb71625f224fb0e7
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-11-05 22:46:29 +0300

    Create ProductsController

---
+++ RealEstate_Dapper.Api/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper.Api.Repositories.ProductRepository;
+
+namespace RealEstate_Dapper.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductList()
+        {
+            var products = await _productRepository.GetAllProductAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("ProductListWithCategory")]
+        public async Task<IActionResult> ProductListWithCategory()
+        {
+            var productsWithCategories = await _productRepository.GetAllProductWithCategoryAsync();
+            return Ok(productsWithCategories);
+        }
+    }
+}
