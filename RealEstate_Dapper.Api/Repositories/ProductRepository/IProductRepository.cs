1064f12296fb276703f2bec47ca17ddf1216d3c7
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-11-05 22:46:12 +0300

    Create IProductRepository

---
+++ RealEstate_Dapper.Api/Repositories/ProductRepository/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using RealEstate_Dapper.Api.Dtos.CategoryDtos;
+using RealEstate_Dapper.Api.Dtos.ProductDtos;
+
+namespace RealEstate_Dapper.Api.Repositories.ProductRepository
+{
+    public interface IProductRepository
+    {
+        Task<List<ResultProductDto>> GetAllProductAsync();
+        Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoryAsync(); 
+    }
+}
