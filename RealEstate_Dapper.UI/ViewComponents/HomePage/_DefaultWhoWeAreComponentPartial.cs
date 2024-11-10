15bec83ccfae208d00d33b66247ef6a3c4cb9016
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-11-10 23:02:00 +0300

    add new components

--- RealEstate_Dapper.UI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
+++ RealEstate_Dapper.UI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
@@ -4,7 +4,7 @@ namespace RealEstate_Dapper.UI.ViewComponents.HomePage
 {
     public class _DefaultWhoWeAreComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult>  InvokeAsync()
         {
             return View();
         }
