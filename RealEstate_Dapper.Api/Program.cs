bc74484f48a101bb176234101094755d4fb51acd
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-11-18 23:44:20 +0300

    update program.cs

--- RealEstate_Dapper.Api/Program.cs
+++ RealEstate_Dapper.Api/Program.cs
@@ -1,6 +1,10 @@
 using RealEstate_Dapper.Api.Models.DapperContext;
+using RealEstate_Dapper.Api.Repositories.BottomGridRepositories;
 using RealEstate_Dapper.Api.Repositories.CategoryRepository;
+using RealEstate_Dapper.Api.Repositories.PopularLocationRepositories;
 using RealEstate_Dapper.Api.Repositories.ProductRepository;
+using RealEstate_Dapper.Api.Repositories.ServiceRepository;
+using RealEstate_Dapper.Api.Repositories.TestimonialRepositories;
 using RealEstate_Dapper.Api.Repositories.WhoWeAreRepository;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +15,10 @@ builder.Services.AddTransient<Context>();
 builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
 builder.Services.AddTransient<IProductRepository, ProductRepository>();
 builder.Services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepository>();
+builder.Services.AddTransient<IServiceRepository, ServiceRepository>();
+builder.Services.AddTransient<IBottomGridRepository, BottomGridRepository>();
+builder.Services.AddTransient<IPopularLocationRepository, PopularLocationRepository>();
+builder.Services.AddTransient<ITestimonialRepository, TestimonialRepository>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
