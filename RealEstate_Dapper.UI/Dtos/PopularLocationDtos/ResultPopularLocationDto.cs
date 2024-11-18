d3a23b8c82f70b7a92f6733e4c37d5d56725c88b
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-11-18 23:47:44 +0300

    add new Dto

---
+++ RealEstate_Dapper.UI/Dtos/PopularLocationDtos/ResultPopularLocationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate_Dapper.UI.Dtos.PopulerLocationsDtos
+{
+    public class ResultPopularLocationDto
+    {
+        public int LocationID { get; set; }
+
+        public string CityName { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ RealEstate_Dapper.UI/Dtos/TestimonialDtos/ResultTestimonialDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate_Dapper.UI.Dtos.TestimonialDtos
+{
+    public class ResultTestimonialDto
+    {
+        public int TestimonialID { get; set; }
+        public string NameSurname { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        bool Status { get; set; }
+    }
+}
