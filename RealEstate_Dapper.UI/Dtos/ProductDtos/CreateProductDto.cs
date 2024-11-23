ba4474f0a3cdb92db4e627743169f306f04ad52f
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-11-24 00:14:50 +0300

    create new product dto

---
+++ RealEstate_Dapper.UI/Dtos/ProductDtos/CreateProductDto.cs
@@ -0,0 +1,14 @@
+﻿namespace RealEstate_Dapper.UI.Dtos.ProductDtos
+{
+    public class CreateProductDto
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string CategoryName { get; set; }
+        public string coverImage { get; set; }
+        public string Type { get; set; }
+        public string Address { get; set; }
+    }
+}
