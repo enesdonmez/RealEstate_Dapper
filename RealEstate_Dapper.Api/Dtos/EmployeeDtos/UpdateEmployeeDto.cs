3f0c26f10f1ef787967ff7104b28af121ba197a4
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-11-26 01:19:02 +0300

    create new dto

---
+++ RealEstate_Dapper.Api/Dtos/EmployeeDtos/UpdateEmployeeDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RealEstate_Dapper.Api.Dtos.EmployeeDtos
+{
+    public class UpdateEmployeeDto
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+    }
+}
