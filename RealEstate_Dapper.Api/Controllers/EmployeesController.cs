46cff7230d6e7c3890e98b3095feca7620fcb12f
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-11-26 01:20:19 +0300

    create employeeController

---
+++ RealEstate_Dapper.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper.Api.Dtos.CategoryDtos;
+using RealEstate_Dapper.Api.Dtos.EmployeeDtos;
+using RealEstate_Dapper.Api.Repositories.EmployeeRepositories;
+
+namespace RealEstate_Dapper.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployeeList()
+        {
+            var values = await _employeeRepository.GetAllEmployeeAsync();
+
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee(CreateEmployeeDto createEmployeeDto)
+        {
+            await _employeeRepository.CreateEmployeeAsync(createEmployeeDto);
+            return Ok("Employee eklendi.");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            await _employeeRepository.DeleteEmployeeAsync(id);
+            return Ok("Employee Silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmployee(UpdateEmployeeDto updateEmployeeDto)
+        {
+            await _employeeRepository.UpdateEmployeeAsync(updateEmployeeDto);
+            return Ok("Güncellendi.");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployeeById(int id)
+        {
+            var employee = await _employeeRepository.GetEmployeeAsync(id);
+            return Ok(employee);
+        }
+    }
+}
