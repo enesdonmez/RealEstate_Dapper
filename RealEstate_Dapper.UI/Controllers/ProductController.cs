24a153d83cccf8ee221111c0a75f5ebd20ef8524
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-11-26 01:19:42 +0300

    .

--- RealEstate_Dapper.UI/Controllers/ProductController.cs
+++ RealEstate_Dapper.UI/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using RealEstate_Dapper.UI.Dtos.CategoryDtos;
 using RealEstate_Dapper.UI.Dtos.ProductDtos;
 
 namespace RealEstate_Dapper.UI.Controllers
@@ -7,7 +9,7 @@ namespace RealEstate_Dapper.UI.Controllers
     public class ProductController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
-        
+
         public ProductController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
@@ -23,6 +25,26 @@ namespace RealEstate_Dapper.UI.Controllers
                 var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
                 return View(values);
             }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateProduct()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:44377/api/Categories");
+
+            var jsonData = await response.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+
+            List<SelectListItem> categoryValues = (from x in values.ToList()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CategoryID.ToString()
+                                                   }).ToList();
+
+            ViewBag.categories = categoryValues;
             return View();
         }
     }
