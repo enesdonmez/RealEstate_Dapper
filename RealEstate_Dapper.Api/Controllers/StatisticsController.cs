bc1e143cca0d91d8d2f8c1b47dd3f8f6d7310911
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-12-13 00:25:32 +0300

    Create StatisticsController.cs

---
+++ RealEstate_Dapper.Api/Controllers/StatisticsController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper.Api.Repositories.StatisticsRepositories;
+
+namespace RealEstate_Dapper.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IStatisticsRepository _statisticsRepository;
+
+        public StatisticsController(IStatisticsRepository statisticsRepository)
+        {
+            _statisticsRepository = statisticsRepository;
+        }
+
+
+        //Aktif kategori sayisi
+        [HttpGet("ActiveCategoryCount")]
+        public IActionResult ActiveCategoryCount()
+        {
+            return Ok(_statisticsRepository.ActiveCategoryCount());
+        }
+
+        //Aktif employee sayisi
+        [HttpGet("ActiveEmployeeCount")]
+        public IActionResult ActiveEmployeeCount()
+        {
+            return Ok(_statisticsRepository.ActiveEmployeeCount());
+        }
+
+        [HttpGet("ApartmentCount")]
+        public IActionResult ApartmentCount()
+        {
+            return Ok(_statisticsRepository.ApartmentCount());
+        }
+
+        [HttpGet("AverageProductPriceByRent")]
+        public IActionResult AverageProductByRent()
+        {
+            return Ok(_statisticsRepository.AverageProductPriceByRent());
+        }
+
+        [HttpGet("AverageProductPriceBySale")]
+        public IActionResult AverageProductPriceBySale()
+        {
+            return Ok(_statisticsRepository.AverageProductPriceBySale());
+        }
+
+        [HttpGet("AverageRoomCount")]
+        public IActionResult AverageRoomCount()
+        {
+            return Ok(_statisticsRepository.AverageRoomCount());
+        }
+
+        [HttpGet("CategoryCount")]
+        public IActionResult CategoryCount()
+        {
+            return Ok(_statisticsRepository.CategoryCount());
+        }
+
+        [HttpGet("CategoryNameByMaxProductCount")]
+        public IActionResult CategoryNameByMaxProductCount()
+        {
+            return Ok(_statisticsRepository.CategoryNameByMaxProductCount());
+        }
+
+        [HttpGet("CityNameByMaxProductCount")]
+        public IActionResult CityNameByMaxProductCount()
+        {
+            return Ok(_statisticsRepository.CityNameByMaxProductCount());
+        }
+
+        [HttpGet("DifferentCityCount")]
+        public IActionResult DifferentCityCount()
+        {
+            return Ok(_statisticsRepository.DifferentCityCount());
+        }
+
+        [HttpGet("EmployeeNameByMaxProductCount")]
+        public IActionResult EmployeeNameByMaxProductCount()
+        {
+            return Ok(_statisticsRepository.EmployeeNameByMaxProductCount());
+        }
+
+        [HttpGet("LastProductPrice")]
+        public IActionResult LastProductPrice()
+        {
+            return Ok(_statisticsRepository.LastProductPrice());
+        }
+
+        [HttpGet("NewestBuildingYear")]
+        public IActionResult NewestBuildingYear()
+        {
+            return Ok(_statisticsRepository.NewestBuildingYear());
+        }
+
+        [HttpGet("OldestBuildingYear")]
+        public IActionResult OldestBuildingYear()
+        {
+            return Ok(_statisticsRepository.OldestBuildingYear());
+        }
+
+        [HttpGet("PasiveCategoryCount")]
+        public IActionResult PasiveCategoryCount()
+        {
+            return Ok(_statisticsRepository.PasiveCategoryCount());
+        }
+
+        [HttpGet("ProductCount")]
+        public IActionResult ProductCount()
+        {
+            return Ok(_statisticsRepository.ProductCount());
+        }
+    }
+}
