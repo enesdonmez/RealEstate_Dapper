6d3f0138fe635b6b2ce4a66ce046b6bbc447844d
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-12-13 00:26:55 +0300

    Create PopularLocationController.cs

---
+++ RealEstate_Dapper.UI/Controllers/PopularLocationController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper.UI.Dtos.PopularLocationsDtos;
+using System.Text;
+
+namespace RealEstate_Dapper.UI.Controllers
+{
+    public class PopularLocationController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PopularLocationController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:44377/api/Popularlocations");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultPopularLocationDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreatePopularLocation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePopularLocation(CreatePopularLocationDto createPopularLocationDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            createPopularLocationDto.ImageUrl = $"/CityImages/{createPopularLocationDto.ImageUrl}";
+            var data = JsonConvert.SerializeObject(createPopularLocationDto);
+            StringContent stringContent = new StringContent(data, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:44377/api/Popularlocations", stringContent);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeletePopularLocation(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.DeleteAsync($"https://localhost:44377/api/PopularLocations/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdatePopularLocation(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:44377/api/PopularLocations/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdatePopularLocationDto>(data);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(UpdatePopularLocationDto updatePopularLocationDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var data = JsonConvert.SerializeObject(updatePopularLocationDto);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync("https://localhost:44377/api/PopularLocations", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+
+            }
+            return View();
+
+        }
+    }
+}
