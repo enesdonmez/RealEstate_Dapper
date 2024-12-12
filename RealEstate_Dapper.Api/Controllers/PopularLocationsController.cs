b072dcc03adea0020a64ab8b59edfdb73b6ce51c
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-12-13 00:25:24 +0300

    Update PopularLocationsController.cs

--- RealEstate_Dapper.Api/Controllers/PopularLocationsController.cs
+++ RealEstate_Dapper.Api/Controllers/PopularLocationsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper.Api.Dtos.PopularLocationsDtos;
 using RealEstate_Dapper.Api.Repositories.PopularLocationRepositories;
 
 namespace RealEstate_Dapper.Api.Controllers
@@ -21,5 +22,34 @@ namespace RealEstate_Dapper.Api.Controllers
             var Locations = await _popularLocationRepository.GetAllPopularLocationAsync(); 
             return Ok(Locations);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePopularLocation(CreatePopularLocationDto createPopularLocationDto)
+        {
+            await _popularLocationRepository.CreatePopularLocationAsync(createPopularLocationDto);
+            return Ok("Ekleme Başarılı.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdatePopularLocation(UpdatePopularLocationDto updatePopularLocationDto)
+        {
+            await _popularLocationRepository.UpdatePopularLocationAsync(updatePopularLocationDto);
+            return Ok("güncelleme Başarılı.");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePopularLocation(int id)
+        {
+            await _popularLocationRepository.DeletePopularLocationAsync(id);
+            return Ok("Lokasyon Silindi.");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPopularLocationById(int id)
+        {
+            var location = await _popularLocationRepository.GetPopularLocationAsync(id);
+            return Ok(location);
+        }
+
     }
 }
