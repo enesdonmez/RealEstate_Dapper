443f5648935b801184ea477533546980314a85b6
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-12-13 00:28:16 +0300

    Create CreatePopularLocationDto.cs

---
+++ RealEstate_Dapper.UI/Dtos/PopularLocationDtos/CreatePopularLocationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate_Dapper.UI.Dtos.PopularLocationsDtos
+{
+    public class CreatePopularLocationDto
+    {
+        
+        public string CityName { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
