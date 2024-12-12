70040f3f4e8cea1e7e4caa40c5cd28be7aa544dd
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-12-13 00:25:28 +0300

    Update ServicesController.cs

--- RealEstate_Dapper.Api/Controllers/ServicesController.cs
+++ RealEstate_Dapper.Api/Controllers/ServicesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper.Api.Dtos.ServiceDtos;
+using RealEstate_Dapper.Api.Dtos.WhoWeAreDetailDtos;
 using RealEstate_Dapper.Api.Repositories.CategoryRepository;
 using RealEstate_Dapper.Api.Repositories.ServiceRepository;
 
@@ -17,10 +19,38 @@ namespace RealEstate_Dapper.Api.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetServiceList() 
+        public async Task<IActionResult> GetServiceList()
         {
             var value = await _serviceRepository.GetAllServiceAsync();
             return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateService(CreateServiceDto createServiceDto)
+        {
+            await _serviceRepository.CreateServiceAsync(createServiceDto);
+            return Ok("Servis eklendi.");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteService(int id)
+        {
+            await _serviceRepository.DeleteServiceAsync(id);
+            return Ok("Servis Silindi.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateService(UpdateServiceDto updateServiceDto)
+        {
+            await _serviceRepository.UpdateServiceAsync(updateServiceDto);
+            return Ok("Servis güncellendi.");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetServiceById(int id)
+        {
+            var service = await _serviceRepository.GetServiceAsync(id);
+            return Ok(service);
         }
     }
 }
