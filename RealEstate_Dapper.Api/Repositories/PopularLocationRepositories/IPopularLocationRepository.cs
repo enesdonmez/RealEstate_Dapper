189894242342c5bbe9bc3c92994423cdea652b65
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-12-13 00:26:38 +0300

    Update IPopularLocationRepository.cs

--- RealEstate_Dapper.Api/Repositories/PopularLocationRepositories/IPopularLocationRepository.cs
+++ RealEstate_Dapper.Api/Repositories/PopularLocationRepositories/IPopularLocationRepository.cs
@@ -1,11 +1,14 @@
-﻿using RealEstate_Dapper.Api.Dtos.BottomGridDtos;
-using RealEstate_Dapper.Api.Dtos.PopulerLocationsDtos;
+﻿using RealEstate_Dapper.Api.Dtos.PopularLocationsDtos;
 
 namespace RealEstate_Dapper.Api.Repositories.PopularLocationRepositories
 {
     public interface IPopularLocationRepository
     {
         Task<List<ResultPopularLocationDto>> GetAllPopularLocationAsync();
-  
+        Task CreatePopularLocationAsync(CreatePopularLocationDto createPopularLocationDto);
+        Task DeletePopularLocationAsync(int id);
+        Task UpdatePopularLocationAsync(UpdatePopularLocationDto updatePopularLocationDto);
+        Task<GetByIDPopularLocationDto> GetPopularLocationAsync(int id);
+
     }
 }
