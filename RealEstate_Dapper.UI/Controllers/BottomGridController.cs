dec098917f5b4a6cea414b8525491700d2a8e4e6
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-12-13 00:26:52 +0300

    Create BottomGridController.cs

---
+++ RealEstate_Dapper.UI/Controllers/BottomGridController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper.UI.Dtos.BottomGridDtos;
+using System.Text;
+
+namespace RealEstate_Dapper.UI.Controllers
+{
+    public class BottomGridController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BottomGridController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44377/api/BottomGrids");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBottomGridDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateBottomGrid()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBottomGrid(CreateBottomGridDto createBottomGridDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var data = JsonConvert.SerializeObject(createBottomGridDto);
+            StringContent stringContent = new StringContent(data, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:44377/api/BottomGrids", stringContent);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteBottomGrid(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.DeleteAsync($"https://localhost:44377/api/BottomGrids/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateBottomGrid(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:44377/api/BottomGrids/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateBottomGridDto>(data);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateBottomGrid(UpdateBottomGridDto updateBottomGridDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var data = JsonConvert.SerializeObject(updateBottomGridDto);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync("https://localhost:44377/api/BottomGrids", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+
+            }
+            return View();
+
+        }
+    }
+}
