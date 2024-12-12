25833564db7a8e5b5fc21d6660aec3d28e10bf63
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-12-13 00:26:47 +0300

    Create IStatisticsRepository.cs

---
+++ RealEstate_Dapper.Api/Repositories/StatisticsRepositories/IStatisticsRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+namespace RealEstate_Dapper.Api.Repositories.StatisticsRepositories
+{
+    public interface IStatisticsRepository
+    {
+        int CategoryCount();  
+        int ActiveCategoryCount();  
+        int PasiveCategoryCount();
+        int ProductCount();
+        int ApartmentCount();
+        string EmployeeNameByMaxProductCount();
+        string CategoryNameByMaxProductCount();
+        decimal AverageProductPriceByRent();
+        decimal AverageProductPriceBySale();
+        string CityNameByMaxProductCount();
+        int DifferentCityCount();
+        decimal LastProductPrice();
+        string NewestBuildingYear();
+        string OldestBuildingYear();
+        int AverageRoomCount();
+        int ActiveEmployeeCount();
+        
+    }
+}
