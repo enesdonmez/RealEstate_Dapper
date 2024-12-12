9ea62269d8a1cee65b4ef774f5976d32ad2c233b
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-12-13 00:26:08 +0300

    Create GetByIDPopularLocationDto.cs

---
+++ RealEstate_Dapper.Api/Dtos/PopularLocationsDtos/GetByIDPopularLocationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate_Dapper.Api.Dtos.PopularLocationsDtos
+{
+    public class GetByIDPopularLocationDto
+    {
+        public int LocationID { get; set; }
+
+        public string CityName { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
