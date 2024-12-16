9c3a01369a479ba0940cfbcd3995596547740985
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-12-17 01:47:23 +0300

    create jwt tools

---
+++ RealEstate_Dapper.Api/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate_Dapper.Api.Tools
+{
+    public class JwtTokenDefaults
+    {
+        public const string ValidAudience = "https://localhost"; //  dinleyecek
+        public const string ValidIssuer = "https://localhost";   //  yayınlayacak 
+        public const string Key = "RealEstate..987654321Asp.NetCore8.0**-";
+        public const int Expire = 5;
+    
+    }
+}
---
+++ RealEstate_Dapper.Api/Tools/JwtTokenGenerator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace RealEstate_Dapper.Api.Tools
+{
+    public class JwtTokenGenerator
+    {
+        public static TokenResponseViewModel GenerateToken(GetCheckAppUserViewModel model)
+        {
+            var claims = new List<Claim>();
+
+            if (!string.IsNullOrWhiteSpace(model.Role))
+                claims.Add(new Claim(ClaimTypes.Role, model.Role));
+
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, model.Id.ToString()));
+
+            if (!string.IsNullOrWhiteSpace(model.UserName))
+                claims.Add(new Claim("Username", model.UserName));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtTokenDefaults.Key));
+
+            var singinCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expireDate = DateTime.UtcNow.AddHours(JwtTokenDefaults.Expire);
+
+            JwtSecurityToken token = new JwtSecurityToken(JwtTokenDefaults.ValidIssuer, JwtTokenDefaults.ValidAudience, claims: claims, notBefore: DateTime.UtcNow, expires: expireDate, signingCredentials:singinCredentials);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+            return new TokenResponseViewModel(handler.WriteToken(token), expireDate);
+        }
+    }
+}
