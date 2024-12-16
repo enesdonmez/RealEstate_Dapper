1be01a8fceed100ca8c5476c6a4c54521425c648
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-12-17 01:47:26 +0300

    Create RolesType.cs

---
+++ RealEstate_Dapper.Api/Enums/RolesType.cs
@@ -0,0 +1,12 @@
+﻿namespace RealEstate_Dapper.Api.RoleTypes
+{
+    public enum RolesType
+    {
+        Admin = 1,
+        Member = 2,
+        Manager = 3,
+        Visitor = 4,
+        Agent = 5
+       
+    }
+}
