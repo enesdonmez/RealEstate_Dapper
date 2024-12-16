57bdcb5c618a9608e0f3b2153abd2b2cb7c2b130
Enes DÖNMEZ <enesd_5034@outlook.com>
2024-12-16 23:38:33 +0300

    Create ContactsController.cs

---
+++ RealEstate_Dapper.Api/Controllers/ContactsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper.Api.Dtos.ContactDtos;
+using RealEstate_Dapper.Api.Repositories.ContactRepositories;
+
+namespace RealEstate_Dapper.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactRepository _contactRepository;
+
+        public ContactsController(IContactRepository contactRepository)
+        {
+            _contactRepository = contactRepository;
+        }
+
+        [HttpGet("GetContactList")]
+        public async Task<IActionResult> GetContactList()
+        {
+            var values = await _contactRepository.GetAllContactAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("GetLastFourContactList")]
+        public async Task<IActionResult> GetLastFourContactList()
+        {
+            var values = await _contactRepository.GetLastFourContactAsync();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateContact(CreateContactDto createContactDto)
+        {
+            await _contactRepository.CreateContactAsync(createContactDto);
+            return Ok("Ekleme Başarılı.");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetContact(int id)
+        {
+            var values = await _contactRepository.GetContactAsync(id);
+            return Ok(values);
+        }
+    }
+}
